18f896654df43fee4862c3e763f40133bfb01a3d
Nico.D <nicolas.drew@hotmail.com>
2025-07-11 16:35:36 +0200

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-left">© 2025 Weather</div>
+      <div className="footer-right">
+        <a href="#">Mentions légales</a>
+        <a href="#">Contact</a>
+        <a href="#">GitHub</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <>
+    <div className="app-container">
       <Navbar />
-      <main>
+      <main className="main-content">
         <Hero />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 };
 
